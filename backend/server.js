a3c87842cac7bd2f431ccc86881a4a34a96dcb46
HeyitzSagar <sagargupta.gola@gmail.com>
2025-03-12 19:30:47 +0530

    ADDED: DB connection

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv');
 dotenv.config();
+const connectDB = require('./db/db')
 const port = process.env.PORT
 
+connectDB();
+app.use(express.json());
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
