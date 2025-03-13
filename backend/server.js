bd9768affedcb90f9843767b69df431bef269ee1
HeyitzSagar <sagargupta.gola@gmail.com>
2025-03-13 12:05:11 +0530

    ADDED: task completed

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const dotenv = require('dotenv');
 const router = require('./routes/api.routes')
 const cors = require('cors');
@@ -10,6 +11,8 @@ const port = process.env.PORT
 connectDB();
 // lets treat it to allow request from any where or origin
 
+// const __dirname = path.resolve();
+
 app.use(cors());
  // middleware to parse JSON request bodies
  app.use(express.urlencoded({ extended: true }));
@@ -18,6 +21,13 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/v1/api', router);
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
+}
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
